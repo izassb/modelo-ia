613190b6ac030e0d885fceaba07ddc6a4cebcbd2
caioabmoura <caio.abezerram@gmail.com>
2023-06-08 00:37:51 -0300

    add route for model

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from PIL import Image
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+app = Flask(__name__)
+CORS(app)
+
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if (os.path.isfile('./model-final.h5')):
+    path = 'model-final.h5'
+else:
+    path = './src/model-final.h5'
+
+model = load_model(path, compile=False)
+model.compile()
+
+def preprocess(img):
+    img = img.resize((224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.
+    return img_array
+
+def get_class_info(prediction):
+    class_id = np.argmax(prediction)
+
+    if class_id == 0:
+        class_name = "feliz"
+    elif class_id == 1:
+        class_name = "triste"
+    else:
+        class_name = "raiva"
+    
+    return int(class_id), class_name
+
+@app.route('/upload', methods=['POST'])
+def predict():
+    file = request.files['file']
+    img = Image.open(file.stream)
+    img_processed = preprocess(img)
+    prediction = model.predict(img_processed)
+    class_id, class_name = get_class_info(prediction)
+    response = { 'type': class_id, 'name': class_name }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
